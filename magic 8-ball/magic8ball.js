1883faf9f5ed19b34198a044843881a2b3d52fe4
rich-caceres <richard.caceres22590@outlook.com>
2019-02-05 10:25:11 -0500

    Magic 8 ball Javascript and page.

---
+++ magic 8-ball/magic8ball.js
@@ -0,0 +1,31 @@
+// JavaScript source code
+
+var magic8ball = {};
+magic8ball.magicAnswer = ["It is certain", "As I see it yes", "Better not tell you now", "Don't count on it"];
+$("#answer").hide();
+magic8ball.quest = function (quest) {
+
+    //create random number
+    var index = Math.floor(Math.random() *
+        magic8ball.magicAnswer.length);
+
+    //use random number for answer
+    var answer = magic8ball.magicAnswer[index];
+    $("#answer").text(" 8 ball says: " + answer);
+
+
+};
+$("#answer").hide();
+var onClick = function () {
+
+    var question = prompt("what is your question");
+    magic8ball.quest(question);
+};
+
+//question to be asked in function
+//magic8ball.question("Will I make millions?");
+$("#questionButton").click(onClick);
+
+
+
+
