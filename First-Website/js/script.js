95f6e0b406d4c96d405dccb89eee95367c4a2205
rich-caceres <richard.caceres22590@outlook.com>
2020-07-01 14:22:59 -0400

    Added logic to show a different message once graduated.

--- First-Website/js/script.js
+++ First-Website/js/script.js
@@ -8,7 +8,13 @@ window.onload = function () {
         var one_day = 1000 * 60 * 60 * 24;
         let today = new Date;
         let gradDay = new Date(2020, 7, 31)
-        let timeLeft = Math.ceil((gradDay.getTime() - today.getTime()) / (one_day));
-        document.getElementById("date").innerHTML = "(" + timeLeft + " days left until graduation!)";
+        let timeLeft;
+        if (gradDay.getTime() <= today.getTime()) {
+            timeLeft = "(He has graduated! Congratulate him on a job well done.)"
+            document.getElementById("date").innerHTML = timeLeft;
+        } else {
+            timeLeft = Math.ceil((gradDay.getTime() - today.getTime()) / (one_day));
+            document.getElementById("date").innerHTML = "(" + timeLeft + " days left until graduation!)";
+        }
     };
 }
