4be150e8034b4d20b4283ef8852fd1add3b767f6
rich-caceres <richard.caceres22590@outlook.com>
2020-07-03 12:03:12 -0400

    Adding slideshow to images with Javascript

--- First-Website/js/script.js
+++ First-Website/js/script.js
@@ -3,21 +3,37 @@
 window.onload = function () {
 
     thisDate();
+    showSlides();
 
-    function thisDate() {
-        var one_day = 1000 * 60 * 60 * 24;
-        let today = new Date;
-        let gradDay = new Date(2020, 7, 31)
-        let timeLeft;
-        let year;
-        if (gradDay.getTime() <= today.getTime()) {
-            timeLeft = "(He has graduated! Congratulate him on a job well done.)"
-            document.getElementById("date").innerHTML = timeLeft;
-        } else {
-            timeLeft = Math.ceil((gradDay.getTime() - today.getTime()) / (one_day));
-            document.getElementById("date").innerHTML = "(" + timeLeft + " days left until graduation!)";
-        }
-        year = today.getFullYear();
-        document.getElementById("footer").innerHTML = "Created by Rich C. Copyright " + year;
-    };
+}
+
+function thisDate() {
+    var one_day = 1000 * 60 * 60 * 24;
+    let today = new Date;
+    let gradDay = new Date(2020, 7, 31)
+    let timeLeft;
+    let year;
+    if (gradDay.getTime() <= today.getTime()) {
+        timeLeft = "(He has graduated! Congratulate him on a job well done.)"
+        document.getElementById("date").innerHTML = timeLeft;
+    } else {
+        timeLeft = Math.ceil((gradDay.getTime() - today.getTime()) / (one_day));
+        document.getElementById("date").innerHTML = "(" + timeLeft + " days left until graduation!)";
+    }
+    year = today.getFullYear();
+    document.getElementById("footer").innerHTML = "Created by Rich C. Copyright " + year;
+}
+
+var slideIndex = 0;
+function showSlides() {
+    
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
