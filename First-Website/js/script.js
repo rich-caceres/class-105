9492db83708891e66894cc6c07f384129e5997f6
rich-caceres <richard.caceres22590@outlook.com>
2020-07-10 17:00:49 -0400

    changed counter to count down to the second

--- First-Website/js/script.js
+++ First-Website/js/script.js
@@ -8,19 +8,36 @@ window.onload = function () {
 }
 
 function thisDate() {
-    var one_day = 1000 * 60 * 60 * 24;
-    let today = new Date;
-    let gradDay = new Date(2020, 7, 31)
-    let timeLeft;
-    let year;
-    if (gradDay.getTime() <= today.getTime()) {
-        timeLeft = "(He has graduated! Congratulate him on a job well done.)"
-        document.getElementById("date").innerHTML = timeLeft;
-    } else {
-        timeLeft = Math.ceil((gradDay.getTime() - today.getTime()) / (one_day));
-        document.getElementById("date").innerHTML = "(" + timeLeft + " days left until graduation!)";
-    }
-    year = today.getFullYear();
+    
+    var today = new Date();
+    var gradDay = new Date("Aug 31, 2020 15:00:00").getTime();
+    
+
+
+    var countDown = setInterval(function () {
+        today = new Date().getTime();
+        var timeLeft = gradDay - today;
+        var day = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        var hour = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minute = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        var second = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+        if (minute < 10) minute = "0" + minute;
+        if (second < 10) second = "0" + second;
+
+        if (gradDay <= today) {
+            clearInterval(countDown);
+            timeLeft = "(He has graduated! Congratulate him on a job well done.)";
+            document.getElementById("date").innerHTML = timeLeft;
+        } else {
+            document.getElementById("date").innerHTML = "(" + day + " days " + hour + ":" + minute + ":" + second + " left until graduation!)";
+        }
+
+    }, 1000);
+
+
+    var year = today.getFullYear();
+   
     document.getElementById("footer").innerHTML = "Created by Rich C. Copyright " + year;
 }
 
