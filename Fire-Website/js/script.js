03e72e5af35ab66d9a708f730e02ab68a181b06e
Richard Caceres <richard.caceres22590@outlook.com>
2020-07-29 10:25:41 -0400

    Responsive slideshow added

--- Fire-Website/js/script.js
+++ Fire-Website/js/script.js
@@ -2,6 +2,7 @@
 
 window.onload = function () {
     thisDate();
+    showSlides(slideIndex);
 }
 
 function thisDate() {
@@ -54,7 +55,7 @@ function thisDate() {
 }
 
 var slideIndex = 0;
-function showSlides() {
+function showSlides1() {
     
     var i;
     var slides = document.getElementsByClassName("slides");
@@ -64,6 +65,30 @@ function showSlides() {
     slideIndex++;
     if (slideIndex > slides.length) { slideIndex = 1 }
     slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 2 seconds
+    setTimeout(showSlides, 4000); // Change image every 4 seconds
 }
 
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
