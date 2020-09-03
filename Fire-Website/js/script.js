267c336b4873a18b4f371276406e895e919f19d8
rich-caceres <richard.caceres22590@outlook.com>
2020-09-03 08:53:33 -0400

    initial creation of loader without animation

--- Fire-Website/js/script.js
+++ Fire-Website/js/script.js
@@ -2,7 +2,7 @@
 
 window.onload = function () {
     thisDate();
-    //showSlides(slideIndex);
+    loader();
 }
 
 function thisDate() {
@@ -54,41 +54,15 @@ function thisDate() {
     document.getElementById("footer").innerHTML = "Created by Rich C. Copyright " + year;
 }
 
-var slideIndex = 0;
-function showSlides1() {
-    
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 4000); // Change image every 4 seconds
+var load;
+function loader() {
+
+    load = setTimeout(loadDiv, 3000);
+
 }
 
+function loadDiv() {
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    document.getElementById("loadingContent").style.display = "none";
+    document.getElementById("carouselControl").style.display = "block";
 }
