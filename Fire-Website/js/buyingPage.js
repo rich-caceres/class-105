baf547ef4669928cb9f9eae4cbc23dd7d6240aae
Richard Caceres <richard.caceres22590@outlook.com>
2020-09-23 19:35:16 -0400

    testing all features of the counter

--- Fire-Website/js/buyingPage.js
+++ Fire-Website/js/buyingPage.js
@@ -1,19 +1,22 @@
+buyingPageCounter();
 
-var shirtsInStock = new Date(2020, 9, 1, 0, 00, 00, 000);
+function buyingPageCounter() {
+    var shirtsInStock = new Date(2020, 9, 1, 0, 00, 00, 000);
 
-var shirtCountdown = setInterval(function () {
-    //gets current time and updates it every second
-    today = new Date();
+    var shirtCountdown = setInterval(function () {
+        //gets current time and updates it every second
+        //today = new Date();
+        today = new Date(2020, 09, 2, 0, 00, 00, 000);
+        //calculate time left until holloween
+        var shirtInStock;
+        shirtInStock = shirtsInStock.getTime() - today.getTime();
 
-    //calculate time left until holloween
-    var shirtInStock;
-    shirtInStock = shirtsInStock.getTime() - today.getTime();
-
-    if (shirtInStock == today) {
-        clearInterval(shirtCountdown);
-    document.getElementById("shirtInStock").innerHTML = "The following shirts are in stock!";
-} else {
-    document.getElementById("shirtInStock").innerHTML = Math.floor(shirtInStock / (1000 * 60 * 60 * 24)) + " days left until shirts are in stock.";
-}
+        if (shirtInStock <= today) {
+            clearInterval(shirtCountdown);
+            document.getElementById("shirtInStock").innerHTML = "Available Shirt Designs";
+        } else {
+            document.getElementById("shirtInStock").innerHTML = Math.floor(shirtInStock / (1000 * 60 * 60 * 24)) + " days left until shirts are in stock.";
+        }
 
     }, 1000);
+}
