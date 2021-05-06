c6c1c00022afe8967155dd7830598eb870c351a7
rich-caceres <richard.caceres22590@outlook.com>
2021-05-06 16:52:34 -0400

    Minifying JS

--- Fire-Website/js/swiperInitFile.js
+++ Fire-Website/js/swiperInitFile.js
@@ -7,34 +7,10 @@ var swiper3 = new Swiper('#carouselSwiper', {speed: 2000,autoplay: {delay: 2500}
 
 var swiper2 = new Swiper("#swiper2", {autoplay: {delay: 3500},pagination: {el: "#specOpsPage",clickable: true,bulletClass: "specOp__item",bulletActiveClass: "is-active"},plugins: [SwiperPluginPagination, SwiperPluginAutoPlay]});
 
-var swiper1 = new Swiper("#apparatusSwiper", {
-	autoplay: {
-		delay: 3500
-	},
+var swiper1 = new Swiper("#apparatusSwiper", {autoplay: {delay: 3500},pagination: {el: ".swiper-plugin-pagination",clickable: true,bulletClass: "swiper-plugin-pagination__item",bulletActiveClass: "is-active"},plugins: [SwiperPluginPagination, SwiperPluginAutoPlay]});
 
-	pagination: {
-		el: ".swiper-plugin-pagination",
-		clickable: true,
-		bulletClass: "swiper-plugin-pagination__item",
-		bulletActiveClass: "is-active"
-	},
-	plugins: [SwiperPluginPagination, SwiperPluginAutoPlay]
+window.addEventListener('resize', function(){swiper3.update(),swiper3.updateSize()})
 
-});
+new ResizeSensor(jQuery('#SPECOP'), () => {swiper2.update(),swiper2.updateSize()})
 
-window.addEventListener('resize', function(){
-
-	swiper3.update(),
-	swiper3.updateSize()
-
-})
-
-new ResizeSensor(jQuery('#SPECOP'), () => {
-	swiper2.update(),
-		swiper2.updateSize()
-})
-
-new ResizeSensor(jQuery('#collapseTwo'), () => {
-	swiper1.update(),
-		swiper1.updateSize()
-})
+new ResizeSensor(jQuery('#collapseTwo'), () => {swiper1.update(),swiper1.updateSize()})
