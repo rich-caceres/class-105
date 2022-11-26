ae4c870a857d235067f96c55029986b07d01b205
Richard Caceres <richard.caceres22590@outlook.com>
2022-11-26 09:32:30 -0500

    commented out the history link and redirected to home page when going there.

--- Fire-Website/js/DynamicStyle.js
+++ Fire-Website/js/DynamicStyle.js
@@ -4,5 +4,7 @@ window.onload = function () {
 
 function Visibility() {
     //TODO set id for history visibility
-    document.getElementById("history").style.display = "none";
+    //document.getElementById("history").style.display = "none";
+
+    window.location.replace("https://www.salemmafire.org/");
 }
